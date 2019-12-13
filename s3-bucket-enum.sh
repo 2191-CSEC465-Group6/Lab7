122136b2441afa5127c0838cb81909917a42fb14
Robert Ellegate <rje6459@rit.edu>
2019-12-13 17:45:49 -0500

    Adding commands to create two s3 buckets

--- s3-bucket-enum.sh
+++ s3-bucket-enum.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+RESOURCE_PREFIX="csec-465-team6"
+
+# create a public s3 bucket
+aws s3api create-bucket --acl public-read --bucket $RESOURCE_PREFIX-public --region us-east-1
+
+# create a private s3 bucket
+aws s3api create-bucket --acl private --bucket $RESOURCE_PREFIX-private --region us-east-1

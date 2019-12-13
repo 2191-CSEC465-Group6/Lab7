1743073bbd80c43f6a0f60c7c828bebb8a122dfb
Robert Ellegate <rje6459@rit.edu>
2019-12-13 17:56:36 -0500

    Saving location of buckets to var

--- s3-bucket-enum.sh
+++ s3-bucket-enum.sh
@@ -2,7 +2,7 @@
 RESOURCE_PREFIX="csec-465-team6"
 
 # create a public s3 bucket
-aws s3api create-bucket --acl public-read --bucket $RESOURCE_PREFIX-public --region us-east-1
+PRIVATE_BUCKET = $(aws s3api create-bucket --acl public-read --bucket $RESOURCE_PREFIX-public --region us-east-1 | jq .Location)
 
 # create a private s3 bucket
-aws s3api create-bucket --acl private --bucket $RESOURCE_PREFIX-private --region us-east-1
+PUBLIC_BUCKET = $(aws s3api create-bucket --acl private --bucket $RESOURCE_PREFIX-private --region us-east-1 | jq .Location)
